6b9e6cdd4014c45deea0b98479737c5568f0eea9
ghulammeeran <ghulammeeranmirza222@gmail.com>
2023-04-02 22:28:24 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var userNumber=+ prompt("enter a number");
+
+if(userNumber>0){
+    console.log("user number is positive");
+}
+else if (userNumber<0) {
+    console.log("user number is negative");
+}
+else{
+    console.log("user number is zero");
+}
